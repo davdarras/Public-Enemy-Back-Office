744217e6883208392cca12b1b1b38533935d5d1d
Eric Thuaud <eric.thuaud@insee.fr>
2021-06-28 10:41:31 +0200

    add props logging

--- src/main/java/fr/insee/publicenemyapi/PublicEnemyApiApplication.java
+++ src/main/java/fr/insee/publicenemyapi/PublicEnemyApiApplication.java
@@ -1,14 +1,45 @@
 package fr.insee.publicenemyapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
+
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
 
 @SpringBootApplication(scanBasePackages = "fr.insee.publicenemyapi")
 public class PublicEnemyApiApplication extends SpringBootServletInitializer {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(PublicEnemyApiApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PublicEnemyApiApplication.class, args);
+	}
+
+
+	@EventListener
+	public void handleContextRefresh(ContextRefreshedEvent event) {
+		final Environment env = event.getApplicationContext().getEnvironment();
+		LOGGER.info("================================ Properties =================================");
+		final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+		StreamSupport.stream(sources.spliterator(), false)
+				.filter(EnumerablePropertySource.class::isInstance)
+				.map(ps -> ((EnumerablePropertySource<?>) ps).getPropertyNames())
+				.flatMap(Arrays::stream)
+				.distinct()
+				.filter(prop -> !(prop.contains("credentials") || prop.contains("password")))
+				.filter(prop -> prop.startsWith("fr.insee") || prop.startsWith("logging") || prop.startsWith("keycloak") || prop.startsWith("spring") || prop.startsWith("application"))
+				.sorted()
+				.forEach(prop -> LOGGER.info("{}: {}", prop, env.getProperty(prop)));
+		LOGGER.info("============================================================================");
 	}
 
 }
---
+++ src/main/java/fr/insee/publicenemyapi/model/SurveyMetadata.java
@@ -0,0 +1,4 @@
+package fr.insee.publicenemyapi.model;
+
+public class SurveyMetadata {
+}
